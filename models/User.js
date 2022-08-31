6176c95551bf63216d35b2625532a6d2494a7fe2
bpiper91 <bpiper91@gmail.com>
2022-08-30 21:13:50 -0400

    added db/schema.sql, added models/ and set up User model

---
+++ models/User.js
@@ -0,0 +1,61 @@
+const { Model, DataTypes } = require('sequelize');
+const bcrypt = require('bcrypt');
+
+class User extends Model {
+    // check user password for login
+    authenticate(userPassword) {
+      return bcrypt.compareSync(userPassword, this.password);
+    };
+  };
+  
+  // create fields/columns for User model
+  User.init(
+    {
+      id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true
+      },
+      username: {
+        type: DataTypes.STRING,
+        allowNull: false
+      },
+      email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true,
+        validate: {
+          isEmail: true
+        }
+      },
+      password: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+          len: [7]
+        }
+      }
+    },
+    {
+      hooks: {
+        // hash password before creating a new user
+        async beforeCreate(newUserCreds) {
+          newUserCreds.password = await bcrypt.hash(newUserData.password, 10);
+          return newUserCreds;
+        },
+        // hash password before updating a user's password
+        async beforeUpdate(updatedUserCreds) {
+          updatedUserCreds.password = await bcrypt.hash(updatedUserCreds.password, 10);
+          return updatedUserCreds;
+        }
+      },
+      sequelize,
+      timestamps: false,
+      freezeTableName: true,
+      underscored: true,
+      modelName: 'user'
+    }
+  );
+  
+  module.exports = User;
