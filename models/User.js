2ecbda29d4bbb7abc3d991731490c2b597d1f1f6
bpiper91 <bpiper91@gmail.com>
2022-08-30 21:51:16 -0400

    set up Post and Comment models + model associations

--- models/User.js
+++ models/User.js
@@ -1,61 +1,62 @@
 const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
 const bcrypt = require('bcrypt');
 
 class User extends Model {
     // check user password for login
     authenticate(userPassword) {
-      return bcrypt.compareSync(userPassword, this.password);
+        return bcrypt.compareSync(userPassword, this.password);
     };
-  };
-  
-  // create fields/columns for User model
-  User.init(
+};
+
+// create fields/columns for User model
+User.init(
     {
-      id: {
-        type: DataTypes.INTEGER,
-        allowNull: false,
-        primaryKey: true,
-        autoIncrement: true
-      },
-      username: {
-        type: DataTypes.STRING,
-        allowNull: false
-      },
-      email: {
-        type: DataTypes.STRING,
-        allowNull: false,
-        unique: true,
-        validate: {
-          isEmail: true
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        username: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        email: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            unique: true,
+            validate: {
+                isEmail: true
+            }
+        },
+        password: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                len: [7]
+            }
         }
-      },
-      password: {
-        type: DataTypes.STRING,
-        allowNull: false,
-        validate: {
-          len: [7]
-        }
-      }
     },
     {
-      hooks: {
-        // hash password before creating a new user
-        async beforeCreate(newUserCreds) {
-          newUserCreds.password = await bcrypt.hash(newUserData.password, 10);
-          return newUserCreds;
+        hooks: {
+            // hash password before creating a new user
+            async beforeCreate(newUserCreds) {
+                newUserCreds.password = await bcrypt.hash(newUserData.password, 10);
+                return newUserCreds;
+            },
+            // hash password before updating a user's password
+            async beforeUpdate(updatedUserCreds) {
+                updatedUserCreds.password = await bcrypt.hash(updatedUserCreds.password, 10);
+                return updatedUserCreds;
+            }
         },
-        // hash password before updating a user's password
-        async beforeUpdate(updatedUserCreds) {
-          updatedUserCreds.password = await bcrypt.hash(updatedUserCreds.password, 10);
-          return updatedUserCreds;
-        }
-      },
-      sequelize,
-      timestamps: false,
-      freezeTableName: true,
-      underscored: true,
-      modelName: 'user'
+        sequelize,
+        timestamps: false,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'user'
     }
-  );
-  
-  module.exports = User;
+);
+
+module.exports = User;
---
+++ server.js
@@ -0,0 +1,10 @@
+const sequelize = require('./config/connection');
+const express = require('express');
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+// turn on connection to database and server
+sequelize.sync({ force: false }).then(() => {
+    app.listen(PORT, () => console.log(`Now listening on port ${PORT}`));
+});
